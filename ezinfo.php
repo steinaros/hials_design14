7e4c6b7deb3be0c6f350ef22518e9d62ec80ac9a
steinaros <ssj@hials.no>
2014-05-13 13:41:45 +0200

    First test

---
+++ ezinfo.php
@@ -0,0 +1,13 @@
+<?php
+class hials_design14Info
+{
+    function info()
+    {
+        return array( 'Name' => "Design for Høgskolen i Ålesund",
+                      'Version' => "1.0.0",
+                      'Copyright' => "© 2014 <a href=\"http://www.hials.no/\">Høgskolen i Ålesund</a>, <a href=\"http://www.ellemelle.no\">ELLE mELLE AS</a>",
+                      'License' => ""
+                      );
+    }
+}
+?>
---
+++ settings/site.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf8"?
+
+[DebugSettings]
+#DebugOutput=enabled
+
+[TemplateSettings]
+#Debug=enabled
+ExtensionAutoloadPath[]=hials_design14
+
+[RegionalSettings]
+TranslationExtensions[]=hials_design14
+
+*/ ?>
