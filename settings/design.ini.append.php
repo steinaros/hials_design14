d5e5075baab4799a206545955bc1e6d7517f4c8a
steinaros <ssj@hials.no>
2014-05-14 13:21:09 +0200

    Added design.ini

---
+++ settings/design.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf8"?
+
+[ExtensionSettings]
+DesignExtensions[]=hials_design14
+
+#[StylesheetSettings]
+#CSSFileList[]=nxchials.css
+
+#[JavaScriptSettings]
+#JavaScriptList[]=core-hials.js
+#JavaScriptList[]=nxccreateobjectdialog.js
+
+*/ ?>
