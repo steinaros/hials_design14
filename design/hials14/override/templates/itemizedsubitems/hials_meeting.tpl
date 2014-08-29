e2b185178a41137143c2e9d536a6d3daf814d45a
steinaros <ssj@hials.no>
2014-08-29 15:08:41 +0200

    Translation fix? Removed year-heading and fixed bugs in meetinglist

--- design/hials14/override/templates/itemizedsubitems/hials_meeting.tpl
+++ design/hials14/override/templates/itemizedsubitems/hials_meeting.tpl
@@ -21,33 +21,28 @@
                                                'offset', $offset,
                                                'sort_by', array( 'attribute', false(), 'hials_meeting/dato' ) ) ) }
     {if $children|count()}
-    {foreach $children as $child}
-        {if ne($child.data_map.dato.content.year, $year)}
-        <h3>{$child.data_map.dato.content.year|wash()}</h3>
-        {set $year = $child.data_map.dato.content.year}
-        <dl>
-        {/if}
-        <dt><time datetime="{$child.data_map.timestamp|datetime('custom', '%Y-%m-%d')}">{$child.data_map.timestamp|i10n('date')}</time></dt>
+    <dl>
+        {foreach $children as $child}
+        <dt><time datetime="{$child.data_map.dato.timestamp|datetime('custom', '%Y-%m-%d')}">{$child.data_map.dato.timestamp|i10n('date')}</time></dt>
         <dd>
         {if or($child.data_map.sakliste.has_content, $child.data_map.sakliste.has_content)}
             <ul>
             {def $file = $child.data_map.sakliste}        
             {if $file.has_content}
-                <li><div class="attribute-{$file.content.mime_type_part}"><a href={concat("content/download/", $file.contentobject_id, "/", $file.id, "/file/", $file.content.original_filename)|ezurl} title="{'Agenda'|i18n('design/hials/meeting')} {$child.data_map.type|wash()|downcase()} {$child.data_map.timestamp|i10n('date')}">{'Agenda'|i18n('design/hials/meeting')} ({$file.content.filesize|si(byte)})</a></li>
+                <li><div class="attribute-{$file.content.mime_type_part}"><a href={concat("content/download/", $file.contentobject_id, "/", $file.id, "/file/", $file.content.original_filename)|ezurl} title="{'Agenda'|i18n('design/hials/meeting')} {$child.data_map.type|wash()|downcase()} {$child.data_map.dato.timestamp|i10n('date')}">{'Agenda'|i18n('design/hials/meeting')} ({$file.content.filesize|si(byte)})</a></li>
             {/if}
             {undef $file}
             {def $file = $child.data_map.protokoll}        
             {if $file.has_content}
-                <li><div class="attribute-{$file.content.mime_type_part}"><a href={concat("content/download/", $file.contentobject_id, "/", $file.id, "/file/", $file.content.original_filename)|ezurl} title="{'Minutes'|i18n('design/hials/meeting')} {$child.data_map.type|wash()|downcase()} {$child.data_map.timestamp|i10n('date')}">{'Minutes'|i18n('design/hials/meeting')} ({$file.content.filesize|si(byte)})</a></li>
+                <li><div class="attribute-{$file.content.mime_type_part}"><a href={concat("content/download/", $file.contentobject_id, "/", $file.id, "/file/", $file.content.original_filename)|ezurl} title="{'Minutes'|i18n('design/hials/meeting')} {$child.data_map.type|wash()|downcase()} {$child.data_map.dato.timestamp|i10n('date')}">{'Minutes'|i18n('design/hials/meeting')} ({$file.content.filesize|si(byte)})</a></li>
             {/if}
             {undef $file}          
         {else}
             <p>{'No documents are published for this meeting'|i18n('design/hials/meeting')}</p>
         {/if}
-        
-        <ul>
-        </ul>
+        </dd>    
     {/foreach}
+    </dl>
     {/if}
     </div>
 </div>
--- translations/nor-NO/translation.ts
+++ translations/nor-NO/translation.ts
@@ -1,81 +1,81 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0"> 
-<context>
-    <name>design/hials/pagelayout</name>
-    <message>
-      <source>Skip to main content</source>
-      <translation>Hopp til hovedinnhold</translation>
-    </message>
-    <message>
-      <source>Contact us</source>
-      <translation>Kontakt oss</translation>
-    </message>
-    <message>
-      <source>Vacant positions</source>
-      <translation>Ledige stillinger</translation>
-    </message>
-    <message>
-      <source>Site map</source>
-      <translation>Nettstedskart</translation>
-    </message>
-    <message>
-      <source>My profile</source>
-      <translation>Min profil</translation>
-    </message>
-    <message>
-      <source>Login</source>
-      <translation>Logg inn</translation>
-    </message>
-    <message>
-      <source>Logout</source>
-      <translation>Logg ut</translation>
-    </message>
-    <message>
-      <source>Username</source>
-      <translation>Brukernavn</translation>
-    </message>
-    <message>
-      <source>Password</source>
-      <translation>Passord</translation>
-    </message>
-    <message>
-      <source>Forgot your password?</source>
-      <translation>Glemt passordet ditt?</translation>
-    </message>
-</context>
-<context>
-    <name>design/hials/utdanning</name>
-    <message>
-        <source>Requirements</source>
-        <translation>Opptakskrav</translation>
-    </message>
-    <message>
-        <source>Application to</source>
-        <translation>S&oslash;knad</translation>
-    </message>
-    <message>
-        <source>Application deadline</source>
-        <translation>S&oslash;knadsfrist</translation>
-    </message>
-    <message>
-        <source>Credits</source>
-        <translation>Studiepoeng</translation>
-    </message>      
-</context>
-<context>
-    <name>design/hials/meeting</name>
-    <message>
-        <source>Agenda</source>
-        <translation>Sakliste</translation>
-    </message>
-    <message>
-        <source>Minutes</source>
-        <translation>Protokoll</translation>
-    </message>
-    <message>
-        <source>No documents are published for this meeting</source>
-        <translation>Ingen dokumenter er publisert for dette m&oslash;tet</translation>
-    </message>
-</context>
+<TS version="2.0">
+    <context>
+        <name>hials/design/pagelayout</name>
+        <message>
+            <source>Skip to main content</source>
+            <translation>Hopp til hovedinnhold</translation>
+        </message>
+        <message>
+            <source>Contact us</source>
+            <translation>Kontakt oss</translation>
+        </message>
+        <message>
+            <source>Vacant positions</source>
+            <translation>Ledige stillinger</translation>
+	    </message>
+	    <message>
+			<source>Site map</source>
+			<translation>Nettstedskart</translation>
+	    </message>
+	    <message>
+			<source>My profile</source>
+			<translation>Min profil</translation>
+		</message>
+		<message>
+			<source>Login</source>
+			<translation>Logg inn</translation>
+		</message>
+		<message>
+			<source>Logout</source>
+			<translation>Logg ut</translation>
+		</message>
+		<message>
+			<source>Username</source>
+			<translation>Brukernavn</translation>
+		</message>
+		<message>
+			<source>Password</source>
+			<translation>Passord</translation>
+		</message>
+		<message>
+			<source>Forgot your password?</source>
+			<translation>Glemt passordet ditt?</translation>
+		</message>        
+    </context>
+	<context>
+	    <name>design/hials/utdanning</name>
+	    <message>
+	        <source>Requirements</source>
+	        <translation>Opptakskrav</translation>
+	    </message>
+	    <message>
+	        <source>Application to</source>
+	        <translation>S&oslash;knad</translation>
+	    </message>
+	    <message>
+	        <source>Application deadline</source>
+	        <translation>S&oslash;knadsfrist</translation>
+	    </message>
+	    <message>
+	        <source>Credits</source>
+	        <translation>Studiepoeng</translation>
+	    </message>      
+	</context>
+	<context>
+	    <name>design/hials/meeting</name>
+	    <message>
+	        <source>Agenda</source>
+	        <translation>Sakliste</translation>
+	    </message>
+	    <message>
+	        <source>Minutes</source>
+	        <translation>Protokoll</translation>
+	    </message>
+	    <message>
+	        <source>No documents are published for this meeting</source>
+	        <translation>Ingen dokumenter er publisert for dette m&oslash;tet</translation>
+	    </message>
+	</context>    
 </TS>
