18e986928402bf68a4086b906a650e4fb04680e8
steinaros <ssj@hials.no>
2014-09-01 14:20:17 +0200

    Remove autoloads Fix ezinfo.php

--- ezinfo.php
+++ ezinfo.php
@@ -3,9 +3,9 @@ class hials_design14Info
 {
     function info()
     {
-        return array( 'Name' => "Design for Høgskolen i Ålesund, by <a href=\"http://www.ellemelle.no\">ELLE mELLE AS</a>",
+        return array( 'Name' => "Design for HÃ¸gskolen i Ã…lesund, by <a href=\"http://www.ellemelle.no\">ELLE mELLE AS</a>",
                       'Version' => "1.0.0",
-                      'Copyright' => "© 2014 <a href=\"http://www.hials.no/\">H&oslash;gskolen i &Aring;lesund</a>",
+                      'Copyright' => "Â©2014 <a href=\"http://www.hials.no/\">H&oslash;gskolen i &Aring;lesund</a>",
                       'License' => ""
                       );
     }
