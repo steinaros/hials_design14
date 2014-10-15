076d4a4cf7d3a67d6ecc7c0a45a3d59a62082eb5
steinaros <ssj@hials.no>
2014-10-15 14:45:59 +0200

    Speed up animation + disable pushdown for mobile

--- design/hials14/javascript/hials.js
+++ design/hials14/javascript/hials.js
@@ -1,11 +1,22 @@
 $('#mainDropdown').on('show.bs.dropdown', function (e) {
 //	var height = $('.dropdown-menu').outerHeight()
-	var height = $(e.target).find('.dropdown-menu').outerHeight()
-	$('main').css('margin-top', height)
-	$(e.target).find('.dropdown-menu').stop(true, true).slideDown(500, 'linear')
+	if (!$('#navbar-collapse-grid').hasClass('in')) {
+		var height = $(e.target).find('.dropdown-menu').outerHeight()
+		$('main').css('margin-top', height)
+		$(e.target).find('.dropdown-menu').slideDown('fast', 'linear')
+	}
 });
 
 $('#mainDropdown').on('hide.bs.dropdown', function (e) {
-	$(e.target).find('.dropdown-menu').stop(true, true).slideUp(500, 'linear')
-    $('main').css('margin-top', 0)
+	if (!$('#navbar-collapse-grid').hasClass('in')) {	
+		$(e.target).find('.dropdown-menu').slideUp('fast', 'linear')
+		$('main').css('margin-top', 0)
+	}
+});
+
+$('#mainDropdown').on('hidden.bs.dropdown', function (e) {
+	//RESET menu
+	$('ul.tabsubmenu').find('.active').removeClass('active');
+	$('#bachelor').parent('li').addClass('active');
+	$('#submenu-bachelor').addClass('active');
 });
