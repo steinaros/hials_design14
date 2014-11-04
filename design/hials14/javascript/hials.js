64631c09e5926a1bd58a220f0f7373998f1794e3
steinaros <ssj@hials.no>
2014-11-04 11:49:19 +0100

    Fix tab-click closing menu

--- design/hials14/javascript/hials.js
+++ design/hials14/javascript/hials.js
@@ -16,7 +16,26 @@ $('#mainDropdown').on('hide.bs.dropdown', function (e) {
 
 $('#mainDropdown').on('hidden.bs.dropdown', function (e) {
 	//RESET menu
-	$('ul.tabsubmenu').find('.active').removeClass('active');
-	$('#bachelor').parent('li').addClass('active');
-	$('#submenu-bachelor').addClass('active');
+	$('#utdanning_tabs').find('.active').removeClass('active');
+	$('#utdanning_tabs .nav-pills :first-child').addClass('active');
+	$('#utdanning_tabs .tab-content :first-child').addClass('active');
+});
+
+$('[data-toggle="pill"]').on('click', function(event) {
+    // Avoid following the href location when clicking
+   	event.preventDefault() 
+    // Avoid having the menu to close when clicking
+    event.stopPropagation()
+	
+	var $menu = $(event.target).closest('.dropdown-menu')
+	var $tabContent = $($menu).find('.tab-content')
+	var $activeTab = $($tabContent).find('div.tab-pane.active')
+	var $newTab = $($tabContent).find(event.target.hash)
+	
+	if (($activeTab.length == 1) && ($activeTab[0].id != $newTab[0].id)) {
+		$($activeTab).removeClass('active')
+		$($newTab).addClass('active')
+		$('#utdanning_tabs').find('li.active').removeClass('active')
+		$(event.target.parentElement).addClass('active');
+	}
 });
